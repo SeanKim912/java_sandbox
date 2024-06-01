64a026dc790a9b037e0233b29a95c662aaac2a16
Sean <seankim912@yahoo.com>
2024-05-31 23:05:12 -0500

    print and comment methods

---
+++ test.java
@@ -0,0 +1,15 @@
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // short for "print line"
+
+        System.out.print("What "); // prints without a newline
+        System.out.print("is ");
+        System.out.print("up?");
+
+        /**
+         * Commenting is similar to other languages, but the syntax in this block is different
+         * because it is a Javadoc comment. They are used to create documentation for APIs.
+         * Might eventually be read by users.
+         */
+    }
+}
