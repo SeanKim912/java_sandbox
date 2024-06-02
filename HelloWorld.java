34327f30ccea9f14ec12301e5090076f191fd051
Sean <seankim912@yahoo.com>
2024-06-01 19:33:28 -0500

    compiler, executable, variable declaration

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,23 @@
-public class HellowWorld {
+public class HelloWorld {
     public static void main(String[] args) {
+        /*
+         * Compile files with command "javac HelloWorld.java".
+         * Transformed into byte code by compiler and executed by
+         * JVM (Java Virtual Machine).
+         *
+         * Do this to catch errors before running the code.
+         *
+         * After file is compiled, command produces an executable class. Thus in this case
+         * you can run "java HelloWorld".
+         */
         System.out.println("Hello, World!"); // short for "print line"
 
         System.out.print("What "); // prints without a newline
         System.out.print("is ");
         System.out.print("up?");
+
+        String name = "Sean Kim";
+        int yearCreated = 1993;
 
         /**
          * Commenting is similar to other languages, but the syntax in this block is different
