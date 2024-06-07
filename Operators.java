5d0ebdb963af5a012faca4b3aea22ab56d1d1842
Sean <seankim912@yahoo.com>
2024-06-06 20:49:21 -0500

    more quirks of arithmetic

--- Operators.java
+++ Operators.java
@@ -1,10 +1,14 @@
 public class Operators {
     public static void main(String[] args) {
-        // Incrementing and decrementing work similarly to Javascript!
+        // Incrementing and decrementing work similarly to Javascript. +=, -=, %= also same.
         int age = 30;
         age++;
         System.out.println(age);
         age--;
         System.out.println(age);
+
+        // Note that code below prints 2 instead of 2.5, as int floors decimal results.
+        int unevenlyDivided = 10/4;
+        System.out.println(unevenlyDivided);
     }
 }
