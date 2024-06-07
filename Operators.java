196369147855bedec8787d1baa73b45f9dbc3338
Sean <seankim912@yahoo.com>
2024-06-07 16:42:28 -0500

    .equals() method

--- Operators.java
+++ Operators.java
@@ -10,5 +10,14 @@ public class Operators {
         // Note that code below prints 2 instead of 2.5, as int floors decimal results.
         int unevenlyDivided = 10/4;
         System.out.println(unevenlyDivided);
+
+        /*
+         * In Java, you cannot compare objects like strings using primitive operators, so you must use the .equals() method.
+         */
+
+        String str1 = "John";
+        String str2 = "Jim";
+
+        System.out.println(str1.equals(str2)); // false
     }
 }
