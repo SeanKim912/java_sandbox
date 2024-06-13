1260d09871da22ee02dc766e66918517fa01ea89
Sean <seankim912@yahoo.com>
2024-06-12 23:47:34 -0500

    constructor parameters

--- Main.java
+++ Main.java
@@ -5,8 +5,13 @@ public class Main {
             constructor has nothing in it, it would default to a memory address referring to
             the walmart instance of the Store class. "walmart" variable is declared as a
             reference type as a opposed to a primitive type (int, boolean, etc.).
+
+            Can set it equal to null to initialize a reference-type value with no reference.
          */
         Store walmart = new Store();
+        System.out.println(walmart);
+
+        walmart = null;
         System.out.println(walmart);
     }
 }
--- Store.java
+++ Store.java
@@ -1,7 +1,22 @@
 public class Store {
     // Constructor
     public Store() {
-        
+        public String name;
+        public String address;
+        public int size = 1000; // Default values when constructor does not have defined parameters
+        public int capacity = 200;
+
+        // Parameters defined below that you can include when initializing new instance.
+        public Store(String title, String location) {
+            name = title;
+            address = location;
+        }
+
+        // Classes can have multiple constructors, differentiated by what parameters they have.
+        public Car(int area, int people) {
+            size = area;
+            capacity = people;
+        }
     }
     public void buyItems() {
 
